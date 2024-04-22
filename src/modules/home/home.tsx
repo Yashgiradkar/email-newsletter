5458d4dd033e34c58e5e08e131e774daeeedd13f
Yashgiradkar <yashgiradkar02@gmail.com>
2024-04-22 15:49:05 +0530

    created landing page

---
+++ src/modules/home/home.tsx
@@ -0,0 +1,23 @@
+import Banner from '@/modules/home/features/banner'
+import Header from '@/shared/widgets/header'
+import Branding from '@/modules/home/features/branding';
+import Benefits from '@/modules/home/features/benefits';
+import FeatureHighlight from '@/modules/home/features/feature.highlight';
+import Pricing from '@/modules/home/features/pricing';
+import Footer from "@/shared/widgets/footer/footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Banner />
+      <Branding/>
+      <Benefits/>
+      <FeatureHighlight/>
+      <Pricing/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default Home;
